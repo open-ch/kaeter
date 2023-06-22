1a3753d0cce219289f87cfb22379c1c4b4ff48b2
pfiaux <patrick@fiaux.dev>
2023-06-22 16:32:30 +0200

    Fixes autorelease path of . not working when combined with repo (#15385)
    GitOrigin-RevId: 677244b35fbe378f9b81f8a0440ddc6a2ea1cf20

--- cmd/autorelease.go
+++ cmd/autorelease.go
@@ -1,6 +1,9 @@
 package cmd
 
 import (
+	"fmt"
+	"path/filepath"
+
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 
@@ -22,31 +25,37 @@ to release on merge.
 - Can be called multiple times for multiple modules to be released
 `,
 		PreRunE: validateAllPathFlags,
-		Run: func(cmd *cobra.Command, args []string) {
+		RunE: func(cmd *cobra.Command, args []string) error {
 			log.Debugf("Viper settings: %v", viper.AllSettings())
 
 			if len(viper.GetStringSlice("path")) != 1 {
 				log.Debugf("Available paths %v", viper.GetStringSlice("path"))
-				log.Fatalln("Invalid number of paths, only 1 path supported for autorelease")
+				return fmt.Errorf("Invalid number of paths, only 1 path supported for autorelease")
 			}
 
 			version, err := cmd.Flags().GetString("version")
 			if err != nil {
-				log.Fatalf("Autorelease unable to parse version: %s\n", err)
+				return fmt.Errorf("Autorelease failed: unable to parse version: %w", err)
+			}
+
+			modulePath, err := resolveModuleAbsPath()
+			if err != nil {
+				return fmt.Errorf("Autorelease failed: %w", err)
 			}
 
 			config := &actions.AutoReleaseConfig{
-				ModulePath:     viper.GetStringSlice("path")[0],
+				ModulePath:     modulePath,
 				RepositoryRef:  viper.GetString("git.main.branch"),
 				RepositoryRoot: viper.GetString("reporoot"),
 				ReleaseVersion: version,
 				SkipLint:       skipLint,
 			}
 
-			err = actions.AutoRelease(config)
+			return actions.AutoRelease(config)
 			if err != nil {
-				log.Fatalf("Autorelease failed: %s\n", err)
+				return fmt.Errorf("Autorelease failed: %w", err)
 			}
+			return nil
 		},
 	}
 
@@ -56,4 +65,14 @@ to release on merge.
 		"Skips validation of the release, use at your own risk for broken builds.")
 
 	return autoreleaseCmd
+}
+
+func resolveModuleAbsPath() (string, error) {
+	rawModulePath := viper.GetStringSlice("path")[0]
+
+	absModulePath, err := filepath.Abs(rawModulePath)
+	if err != nil {
+		return "", fmt.Errorf("Unable to resolve absolute path from %s: %w", rawModulePath, err)
+	}
+	return absModulePath, nil
 }
--- log/log.go
+++ log/log.go
@@ -22,50 +22,67 @@ func IsDebug() bool {
 // Wrappers... added as needed
 //revive:disable:exported
 
+func Debug(message string, args ...any) {
+	log.Helper()
+	log.Debugf(message, args...)
+}
+
 func Debugln(msg any, keyvals ...any) {
+	log.Helper()
 	// no *ln in charm map to regular
 	log.Debug(msg, keyvals...)
 }
 
 func Debugf(message string, args ...any) {
+	log.Helper()
 	log.Debugf(message, args...)
 }
 
 func Info(msg any, keyvals ...any) {
+	log.Helper()
 	log.Info(msg, keyvals...)
 }
 
 func Infoln(msg any, keyvals ...any) {
+	log.Helper()
 	// no *ln in charm map to regular
 	log.Info(msg, keyvals...)
 }
 
 func Infof(message string, args ...any) {
+	log.Helper()
+
 	log.Infof(message, args...)
 }
 
 func Warnf(message string, args ...any) {
+	log.Helper()
 	log.Warnf(message, args...)
 }
 
 func Errorln(msg any, keyvals ...any) {
+	log.Helper()
 	// no *ln in charm map to regular
 	log.Error(msg, keyvals...)
 }
 
 func Errorf(message string, args ...any) {
+	log.Helper()
 	log.Errorf(message, args...)
 }
 
 func Fatal(msg any, keyvals ...any) {
+	log.Helper()
 	log.Fatal(msg, keyvals...)
 }
 
 func Fatalln(msg any, keyvals ...any) {
+	log.Helper()
 	// no *ln in charm map to regular
 	log.Fatal(msg, keyvals...)
 }
 
 func Fatalf(message string, args ...any) {
+	log.Helper()
 	log.Fatalf(message, args...)
 }
